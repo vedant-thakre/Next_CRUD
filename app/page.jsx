c794a0b530fadfc8788d1290d3b8480789d86794
ved-thakre <vedantthakre7@gmail.com>
2024-01-23 23:12:35 +0530

    integrated get task api

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,29 @@
+"use client"
 import TopicsList from '@/components/TopicsList'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const page = () => {
+    const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/api/task");
+      const obj =  res.data;
+      setData(obj.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
-    <TopicsList/>
+      data.map((task, i) => (
+        <TopicsList key = {i} tit = {task.title} desc = {task.description} />
+      ))
   )
 }
 
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+"use client"
+import React, { useState, useEffect } from 'react'
 import RemoveBtn from './RemoveBtn'
 import Link from 'next/link'
 import { HiPencilAlt } from 'react-icons/hi'
+import axios from 'axios'
 
-const TopicsList = () => {
+const TopicsList = ( { k , tit, desc}) => {
+
+
+  
+
   return (
     <div className='p-4 border rounded-md border-slate-300 my-3 flex justify-between gap-5 items-start'>
-        <div  >
-            <h2 className='font-bold text-2xl' > Task Title</h2>
-            <div>Task Description</div>
-        </div>
+          <div key={k}>
+              <h2 className='font-bold text-2xl' >{tit}</h2>
+              <div>{desc}</div>
+          </div>
 
         <div className='flex gap-2' >
             <RemoveBtn/>
