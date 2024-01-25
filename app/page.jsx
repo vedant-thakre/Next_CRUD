15e900030727188e6838d0231f1c3be7fb2f9f33
ved-thakre <vedantthakre7@gmail.com>
2024-01-25 16:00:03 +0530

    deplyment fixes

--- app/page.jsx
+++ app/page.jsx
@@ -1,30 +1,28 @@
-"use client"
-import TopicsList from '@/components/TopicsList'
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+"use client";
+import TopicsList from "@/components/TopicsList";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const page = () => {
-    const [data, setData] = useState([]);
+const Page = () => {
+  const [data, setData] = useState([]);
 
   const getData = async () => {
     try {
       const res = await axios.get("http://localhost:3000/api/task");
-      const obj =  res.data;
+      const obj = res.data;
       setData(obj.data);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     getData();
-  }, [data])
+  }, [data]);
 
-  return (
-      data.map((task, i) => (
-        <TopicsList key = {i} id= { task._id} tit = {task.title} desc = {task.description} />
-      ))
-  )
-}
+  return data.map((task, i) => (
+    <TopicsList key={i} id={task._id} tit={task.title} desc={task.description} />
+  ));
+};
 
-export default page
+export default Page;
